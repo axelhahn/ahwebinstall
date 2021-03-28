2570a33df2450c9972fc157bf406b81b96702b80
Axel Hahn <ax2002@gmx.net>
2021-03-28 23:03:05 +0200

    update installer; add updatecheck

--- classes/ahwi-installer.class.php
+++ classes/ahwi-installer.class.php
@@ -20,12 +20,11 @@ class ahwi {
     // ----------------------------------------------------------------------
     // INTERNAL CONFIG
     // ----------------------------------------------------------------------
-    var $aCfg = array();
-    var $iTimeStart = false;
-    var $sAbout = "PHP WEB INSTALLER";
-    var $aErrors = array();
-    var $aOK = array();
-    var $sUserAgent = 'php-curl :: axels web installer - see https://github.com/axelhahn/ahwebinstall';
+    protected $aCfg = array();
+    protected $iTimeStart = false;
+    protected $sAbout = "AXELS PHP WEB INSTALLER";
+    protected $aErrors = array();
+    protected $aChecks = array();
 
     // ----------------------------------------------------------------------
     // METHODS
@@ -58,7 +57,7 @@ class ahwi {
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-        curl_setopt($ch, CURLOPT_USERAGENT, $sUserAgent);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'php-curl :: '.$this->sAbout);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
@@ -206,6 +205,24 @@ class ahwi {
     public function getProduct(){
         return $this->aCfg['product'];
     }
+    /**
+     * get array with all requirements, found values and success.
+     * @see getRequirementErrors() to get a list of errors only
+     * @return array
+     */
+    public function getRequirements(){
+        $this->checkRequirements();
+        return $this->aChecks;
+    }
+    /**
+     * get errors if method checkRequirements() failed.
+     * @see getRequirements() to get a list of all requirements and their success
+     * @return array
+     */
+    public function getRequirementErrors(){
+        $this->checkRequirements();
+        return $this->aErrors;
+    }
     
     /**
      * get url of new sources
@@ -249,31 +266,26 @@ class ahwi {
     * check if a php module was found
     * @return boolean
     */
-    function _checkPhpModule($sMod){
-        $aAllMods=get_loaded_extensions(false);
-        asort($aAllMods);
-
-        if(!array_search($sMod, $aAllMods)===false){
-            // echo  '<span class="ok">OK</span> installed';
-            return true;
-        } else {
-            // echo '<span class="error">does not exist</span>';
-            $this->aErrors[]="The PHP module $sMod was not found.";
-            return false;
-        }
-    }
-    
-    /**
-    * check if a php module was found
-    * @return boolean
-    */
-    function _checkModules($aRequiredMods=array()){
+    function _checkModules(){
         if (isset($this->aCfg['checks']['phpextensions']) 
             && is_array($this->aCfg['checks']['phpextensions'])
             && count($this->aCfg['checks']['phpextensions'])){
             
+            $aAllMods=get_loaded_extensions(false);
+            asort($aAllMods);
+            // echo '<pre>aAllMods = '.print_r($aAllMods, 1).'</pre>';
+            
             foreach($this->aCfg['checks']['phpextensions'] as $sMod){
-                $this->_checkPhpModule($sMod);
+                // echo $sMod.' - ';
+                $bModuleExists=!array_search($sMod, $aAllMods)===false;
+                $this->aChecks['phpextensions'][$sMod]=array(
+                    'required'=>'1',
+                    'value'=>$bModuleExists,
+                    'result'=>$bModuleExists,
+                );
+                if(!$bModuleExists){
+                    $this->aErrors[]="php module $sMod was not found";
+                }
             }
         }
 
@@ -283,7 +295,7 @@ class ahwi {
     * check php version 
     * @return boolean
     */
-    function _checkPhpversion($aRequiredMods=array()){
+    function _checkPhpversion(){
         if (isset($this->aCfg['checks']['phpversion']) 
             && $this->aCfg['checks']['phpversion']
             && version_compare(phpversion(), $this->aCfg['checks']['phpversion'],'<')){
@@ -307,22 +319,36 @@ class ahwi {
      */
     function checkRequirements() {
         $this->aErrors=array();
+        $this->aChecks=array();
+        /*
         if(!$this->aCfg['source']){
             return true;
         }
+         */
+        $this->aChecks['phpversion']=array(
+            'required'=>isset($this->aCfg['checks']['phpversion']) ? $this->aCfg['checks']['phpversion'] : false,
+            'value'=>phpversion(),
+            'result'=>'?',
+        );
         if (isset($this->aCfg['checks']['phpversion']) 
             && $this->aCfg['checks']['phpversion']
-            && version_compare(phpversion(), $this->aCfg['checks']['phpversion'],'<')){
+            && version_compare(phpversion(), $this->aCfg['checks']['phpversion'],'<')
+        ){
             $this->aErrors[]="Your PHP version is ".phpversion()."; required: ".$this->aCfg['checks']['phpversion'];
+            $this->aChecks['phpversion']['result']=false;
+        } else {
+            $this->aChecks['phpversion']['result']=true;
         }
         $this->_checkModules();
         $this->_checkDenyroot();
 
         if(count($this->aErrors)){
-            echo "Check for requirements failed.\n* ";
-            echo implode("\n* ", $this->aErrors);
-            echo "\nABORTING.\n";
-            die();
+            if (php_sapi_name() == "cli") {
+                echo "Check for requirements failed.\n";
+                echo implode("\n*", $this->aErrors);
+                die();
+            }
+            return false;
         }
         return true;
     }
@@ -348,7 +374,7 @@ class ahwi {
             die("FATAL ERROR: download won\'t be started. Direcory is not writable: ".dirname($sZipfile).".\n");
         }
 
-        echo "INFO: fetching url $sUrl ...\n";
+        echo "INFO: fetching software url $sUrl ...\n";
         $sData = $this->_httpGet($sUrl);
         echo 'INFO: size is '.strlen($sData) . " byte\n";
         
@@ -360,11 +386,47 @@ class ahwi {
             die("ERROR: unable to store download file $sZipfile.\n");
         } else {
             echo "INFO: download was saved as: $sZipfile\n";
+            if (!$this->verifyDownload(md5_file($sZipfile))){
+                echo "INFO: Verification failed. Removing Download file $sZipfile.\n";
+                unlink($sZipfile);
+                die("ERROR: download file was not as expected.\n");
+            }
         }
-
         return true;
     }
+    
+    /**
+     * verify checksum of download data
+     * @param string  $md5OfData  md5 hash (see method download())
+     * @param string  $sUrl       optional: force an md5 url
+     * @return boolean
+     */
+    function verifyDownload($md5OfData, $sUrl=false) {
+        $sMd5Url=($sUrl 
+                ? $sUrl 
+                : (isset($this->aCfg['md5']) && $this->aCfg['md5']))
+                    ? $this->aCfg['md5']
+                    : $this->aCfg['source'].'.md5'
+                ;
+        echo "INFO: fetching url for checksum $sMd5Url ...\n";
+        $sData = trim($this->_httpGet($sMd5Url));
+        echo 'INFO: size is '.strlen($sData) . " byte\n";
 
+        // if(strlen($sData)!=30 || strlen($sData)>36){
+        if(strlen($sData)!=32){
+            echo "INFO: Ooops, no md5 hash here - skipping md5 check ...\n";
+            return !$this->aCfg['md5'];
+        }
+        echo "INFO: checksums are ...\n"
+                . "  Download: $md5OfData\n"
+                . "  Required: $sData\n";
+        if(strtolower($sData)!=strtolower($md5OfData)){
+            echo "ERROR: checksums do not match\n";
+            return false;
+        }
+        echo "INFO: Checksum is OK.\n";
+        return true;
+    }
     /**
      * install/ unzip method; extract downloaded zip file into target directory
      * if the installation was successful the zip file will be deleted
@@ -376,16 +438,9 @@ class ahwi {
             die("FATAL ERROR: install won\'t be started. Zip file does not exist: $sZipfile.\n");
         }
         
-        $sTargetPath = getcwd().'/'.$this->aCfg['installdir'];
+        $sTargetPath = $this->aCfg['installdir'];
         if (is_dir($sTargetPath)) {
-            echo "INFO: target directory [$sTargetPath] already exists. Making an update.\n";
-        } else {
-            echo "INFO: creating target directory [$sTargetPath]...\n";
-            if(mkdir(dirname($sTargetPath), 0750)){
-                echo "INFO: OK, created.\n";
-            } else {
-                die("FATAL ERROR: unable to create directory $sTargetPath.\n");
-            }
+            echo "INFO: target directory already exists. Making an update.\n";
         }
         if(!is_writable($sTargetPath)){
             die("FATAL ERROR: install won\'t be started. Direcory is not writable: $sTargetPath.\n");
@@ -445,14 +500,6 @@ class ahwi {
     function welcome() {
         echo "
 ===== " . $this->sAbout . " [" . $this->aCfg['product'] . "] =====
-
-";
-
-        $this->_checkPhpModule('curl');
-        $this->_checkPhpModule('zip');
-        $this->checkRequirements();
-
-echo"
 
 What happens next:
 
